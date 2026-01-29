c80172fc45ef93dff8356bb0fdd7dab6875e77be
J Holman <70997898+JNHolman@users.noreply.github.com>
2026-01-29 13:28:31 -0500

    Update providers.tf

--- infrastructure/project-1-cloud-platform/providers.tf
+++ infrastructure/project-1-cloud-platform/providers.tf
@@ -20,4 +20,12 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      Owner       = "Josh"
+    }
+  }
 }
